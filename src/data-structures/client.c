9d35ffb4af5dd124ff53eca488e093b1f9dbf6c3
antropez <spaceraiders@tutanota.com>
2021-11-21 15:21:13 +0000

    feat: finished client data structure operations

--- src/data-structures/client.c
+++ src/data-structures/client.c
@@ -1,3 +1,17 @@
 /*
  * Implementations of the client array and client-related functions.
 */
+
+#include "client.h"
+#include "structures/array.h"
+
+void libserver_client_free(struct LibServerClient client) {
+    return;
+}
+
+/* Macro generated functions */
+ct_array_define_init(LibServerClientArray, struct LibServerClient, libserver_client)
+
+ct_array_define_free(LibServerClientArray, libserver_client, libserver_client_free)
+
+ct_array_define_append(LibServerClientArray, struct LibServerClient, libserver_client)
--- src/data-structures/client.h
+++ src/data-structures/client.h
@@ -12,17 +12,38 @@ struct LibServerClient {
 };
 
 /*
+ * Release a client from memory.
+ *
+ * @param client: the client to release
+*/
+void libserver_client_free(struct LibServerClient client);
+
+/*
  * An array of clients for the server to manage.
 */
 ct_array_define_array(LibServerClientArray, struct LibServerClient);
 
+/*
+ * Initialize a new client array on the heap.
+ *
+ * @parreturn: the new client array
+*/
+struct LibServerClientArray *libserver_client_array_init();
+
+/*
+ * Release a client array and its contents from memory.
+ *
+ * @param array: the array to release
+*/
+void libserver_client_array_free(struct LibServerClientArray *array);
 
 /*
  * Appends a new client onto an array.
  *
  * @param array: the array to append to
  * @param value: the client to append
+ * @return: the array appended to
 */
-void libserver_client_array_append(struct LibServerClientArray *array, struct LibServerClient value);
+struct LibServerClientArray *libserver_client_array_append(struct LibServerClientArray *array, struct LibServerClient value);
 
 #endif
