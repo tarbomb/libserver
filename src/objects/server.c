f03cb184af527bd2311ae99c7762e03c74f22e0c
antropez <spaceraiders@tutanota.com>
2021-11-27 16:17:14 +0000

    feat: added init_mutex function to server

--- src/objects/server.c
+++ src/objects/server.c
@@ -2,8 +2,11 @@
  * Implementations of server functions.
 */
 
+#include <pthread.h>
+
 #include "server.h"
 #include "../libsocket/libsocket.h"
+#include "../shm-tools/shm-tools.h"
 
 struct LibserverServer libserver_server_init(int port, size_t length, struct pollfd clients[]) {
     struct LibserverServer new_server = {0};
@@ -12,4 +15,14 @@ struct LibserverServer libserver_server_init(int port, size_t length, struct pol
     new_server.clients = libserver_client_array_init(length, clients);
 
     return new_server;
+}
+
+pthread_mutex_t *libserver_server_init_mutex(struct LibserverServer *server, const char *file) {
+    pthread_mutex_t *new_mutex = NULL;
+    int mutex_key = shmtools_get_id_create(file, sizeof(pthread_mutex_t));
+    
+    new_mutex = (pthread_mutex_t *) shmtools_attach(mutex_key);
+    server->mutex = new_mutex;
+
+    return new_mutex;
 }
--- src/objects/server.h
+++ src/objects/server.h
@@ -2,9 +2,12 @@
 #define LIB_SERVER_H
 
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "client.h"
 #include "../libsocket/libsocket.h"
+
+#define LIB_SERVER_MAXIMUM_CLIENTS  128
 
 /*
  * The server structure. Contains an array of connected clients,
@@ -13,6 +16,7 @@
 struct LibserverServer {
     struct LibsocketSocket socket;
     struct LibserverClientArray clients;
+    pthread_mutex_t *mutex;
 };
 
 /*
@@ -24,5 +28,15 @@ struct LibserverServer {
  * @return: a new server
 */
 struct LibserverServer libserver_server_init(int port, size_t length, struct pollfd clients[]);
+
+/*
+ * Initializes a new shared-memory mutex into a server, using the file at
+ * the path FILE. The file is created if it does not exist.
+ *
+ * @param server: the server to initialize the mutex into
+ * @param file: the location to store the mutex in
+ * @return: the new mutex
+*/
+pthread_mutex_t *libserver_server_init_mutex(struct LibserverServer *server, const char *file);
 
 #endif
