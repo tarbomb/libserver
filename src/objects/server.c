4a774c9661d1a0e6ccf5bec52d2d9c7e50a3106c
antropez <spaceraiders@tutanota.com>
2021-11-27 15:53:25 +0000

    feat: finished server initialization

--- src/objects/server.c
+++ src/objects/server.c
@@ -3,11 +3,13 @@
 */
 
 #include "server.h"
+#include "../libsocket/libsocket.h"
 
-struct LibserverServer libserver_server_init(int port) {
+struct LibserverServer libserver_server_init(int port, size_t length, struct pollfd clients[]) {
     struct LibserverServer new_server = {0};
 
-    new_server.clients
+    new_server.socket = libsocket_socket_init(port);
+    new_server.clients = libserver_client_array_init(length, clients);
 
     return new_server;
 }
--- src/objects/server.h
+++ src/objects/server.h
@@ -1,14 +1,17 @@
 #ifndef LIB_SERVER_H
 #define LIB_SERVER_H
 
+#include <stdlib.h>
+
 #include "client.h"
+#include "../libsocket/libsocket.h"
 
 /*
  * The server structure. Contains an array of connected clients,
  * as well as the central socket for the server.
 */
 struct LibserverServer {
-    int socket;
+    struct LibsocketSocket socket;
     struct LibserverClientArray clients;
 };
 
@@ -16,8 +19,10 @@ struct LibserverServer {
  * Initialize a new server structure on the stack.
  *
  * @param port: the port of the server
+ * @param length: maximum length of the array
+ * @param clients: pointer to an array of clients
  * @return: a new server
 */
-struct LibserverServer libserver_server_init(int port);
+struct LibserverServer libserver_server_init(int port, size_t length, struct pollfd clients[]);
 
 #endif
